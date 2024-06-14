1f731bb3d866768f855a1ef5df6618b271d83ec6
DESKTOP-5QCQA0B\lucia <lucia_17@naver.com>
2024-06-14 12:33:49 +0900

    step10 Boj Solving

---
+++ src/step10/step10_27323.java
@@ -0,0 +1,13 @@
+package step10;
+
+import java.util.Scanner;
+
+public class step10_27323 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a*b);
+    }
+}
