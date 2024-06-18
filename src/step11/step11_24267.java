400e5c896a260e949b799cd41f93707eb2f6ce65
DESKTOP-5QCQA0B\lucia <lucia_17@naver.com>
2024-06-18 10:44:28 +0900

    step12 Boj Solving

--- src/step11/step11_24267.java
+++ src/step11/step11_24267.java
@@ -8,8 +8,7 @@ public class step11_24267 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
-        Long n = Long.parseLong(br.readLine()); // int는 불가
-
+        long n = Long.parseLong(br.readLine()); // int는 불가
 
         System.out.println( n*(n-1)*(n-2)/6);   // n개의 수 중 i,j,k 3개를 중복없이 고르는 조합
         System.out.println(3);    // O(n^3)의 시간복잡도이므로 최고차항은 3차
